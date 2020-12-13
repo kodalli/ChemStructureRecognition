8f268e02a36cdd8f824d3b4f2d8d701b90620504
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2020-12-12 21:58:45 -0500

    updated gitignore and refactored code

--- src/lines.py
+++ src/lines.py
@@ -2,19 +2,15 @@ import cv2 as cv
 import numpy as np
 
 
-class DrawLines:
-    def __init__(self):
-        pass
+def draw_lines(img, line_color, line_thickness, lines_array):
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
 
-    def Draw(self, img, line_color, line_thickness, lines_array):
-        line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    if lines_array is not None:
+        for line in lines_array:
+            for x1, y1, x2, y2 in line:
+                cv.line(line_img, (x1, y1), (x2, y2),
+                        line_color, line_thickness)
+                # cv.circle(line_img, (x1, y1), 3, [255, 0, 0], 1)
+                # cv.circle(line_img, (x2, y2), 3, [255, 0, 0], 1)
 
-        if lines_array is not None:
-            for line in lines_array:
-                for x1, y1, x2, y2 in line:
-                    cv.line(line_img, (x1, y1), (x2, y2),
-                            line_color, line_thickness)
-                    # cv.circle(line_img, (x1, y1), 3, [255, 0, 0], 1)
-                    # cv.circle(line_img, (x2, y2), 3, [255, 0, 0], 1)
-
-        return line_img, lines_array
+    return line_img, lines_array
--- src/main.py
+++ src/main.py
@@ -1,23 +1,25 @@
 import cv2 as cv
 import numpy as np
-
-from lines import *
-from corners import *
+from lines import draw_lines
+from corners import draw_corners
 from testConnected import find_connected_corners
 
 WINDOW_SIZE = (1024, 1024, 3)
+COLOR_WHITE = (255, 255, 255)
+COLOR_RED = (0, 0, 255)
+COLOR_GREEN = (0, 255, 0)
+
 
 # mouse callback function
 def line_drawing(event, x, y, flags, param):
-    global pt1_x, pt1_y
+    global px, py
     if event == cv.EVENT_LBUTTONDOWN:
-        pt1_x, pt1_y = x, y
-        cv.circle(sketch, (x, y), 3, [0, 0, 255], 5, cv.FILLED)
+        px, py = x, y
+        cv.circle(sketch, (x, y), 3, COLOR_RED, 5, cv.FILLED)
     elif event == cv.EVENT_LBUTTONUP:
-        cv.line(sketch, (pt1_x, pt1_y), (x, y),
-                color=(255, 255, 255), thickness=2)
-        cv.circle(sketch, (x, y), 3, [0, 0, 255], 5, cv.FILLED)
-
+        cv.line(sketch, (px, py), (x, y),
+                color=COLOR_WHITE, thickness=2)
+        cv.circle(sketch, (x, y), 3, COLOR_RED, 5, cv.FILLED)
 
 
 # Create a black image, a window and bind the function to window
@@ -36,13 +38,11 @@ while (1):
     lines = cv.HoughLinesP(edges, rho=1.0, theta=np.pi / 180,
                            threshold=40, minLineLength=10, maxLineGap=10)
 
-    # draw lines and stores endpoints coords in lines_array
-    line_img = DrawLines()
-    line_img, lines_array = line_img.Draw(sketch, [0, 255, 0], 2, lines)
+    # draw lines and stores endpoints coordinates in lines_array
+    line_img, lines_array = draw_lines(sketch, COLOR_GREEN, 2, lines)
 
-    # draw corners and stores coords in corners_array
-    corners_img = DrawCorners()
-    corners_img, corners_array = corners_img.Draw(sketch, line_img,
+    # draw corners and stores coordinates in corners_array
+    corners_img, corners_array = draw_corners(sketch, line_img,
                                                   100, 0.5, 30)
 
     # display corners in new window corners
@@ -58,6 +58,6 @@ while (1):
     elif k == 27:
         break
     elif k == ord('n'):
-        find_connected_corners(corners_array, lines_array, corners_img)
+        find_connected_corners(corners_array, lines_array)
 
 cv.destroyAllWindows()
