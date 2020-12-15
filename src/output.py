64dde192e553cad37ae913bd121f18d5d2a0d2b0
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2020-12-14 19:37:01 -0500

    Polygons are now drawn to scale with average bond length and are centered at their relative location

--- src/output.py
+++ src/output.py
@@ -6,26 +6,42 @@ import math
 
 def draw_molecule(corners_array, graph: Graph, img):
     cycles = graph.get_all_cycles()
-
+    img = np.zeros(img.shape)
     if cycles is not None:
-        draw_first_cycle(cycles[0], corners_array, img)
+        for cycle in cycles:
+            img = draw_first_cycle(
+                cycle, [corners_array[i] for i in cycle], img)
+    cv.imshow("drawn", img)
 
 
 def draw_first_cycle(cycle, corners, img):
-    n = len(cycle)  # number of sides
-    deg = 360/n
-    center = (500, 500)
-    bond_length = 200
+    corners = np.array(corners)
+    corners = corners.reshape((corners.shape[0], 2))
+    x, y = np.mean(corners, axis=0)
+    center = (int(x), int(y))
+
+    cv.circle(img, center, 10, (0, 0, 255))
+
+    bond_length = int(math.hypot(
+        corners[0][0] - center[0], corners[0][1] - center[1]))
+
+    deg = 360/len(cycle)
     points = []
     for i in range(len(cycle)):
         x = bond_length*math.cos(np.deg2rad(deg*i)) + center[0]
-        y = bond_length*math.sin(np.deg2rad(deg*i)) + center[0]
+        y = bond_length*math.sin(np.deg2rad(deg*i)) + center[1]
 
         points.append([int(x), int(y)])
-        cv.circle(img, (points[i][0], points[i][1]), 10, (0, 0, 255))
+        # cv.circle(img, (points[i][0], points[i][1]), 10, (0, 0, 255))
 
     points = np.array(points)
     img = cv.polylines(img, [points],
                        True, (0, 255, 0), 2)
-    cv.circle(img, center, bond_length, (255, 0, 0))
-    return points
+    # cv.circle(img, center, bond_length, (255, 0, 0))
+    return img
+
+
+def find_center(corners):
+    corners = corners.reshape(corners.shape[0], 2)
+    x, y = np.mean(corners, axis=0)
+    return (int(x), int(y))
