b7166a6f12c155824900a54d04f0396ddfd055af
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2020-12-17 19:16:12 -0500

    update requirements

--- src/output.py
+++ src/output.py
@@ -16,7 +16,7 @@ def draw_molecule(corners_array, graph: Graph, img):
 
     bond_length = graph.get_average_bond_length(
         corners_array.reshape((corners_array.shape[0], 2)))
-    
+
     connected_cycles_dict = defaultdict(set)
     for cy1, cy2 in connected_cycles:  # indexes in cycles
         z = list(set(cycles[cy1]).intersection(set(cycles[cy2])))
@@ -26,7 +26,7 @@ def draw_molecule(corners_array, graph: Graph, img):
         connected_cycles_dict[cy1].add(cy2)
         connected_cycles_dict[cy2].add(cy1)
 
-    img = np.zeros(img.shape)   
+    img = np.zeros(img.shape)
 
     drawn = set()
     drawn_cycle_pts_dict = {}
@@ -39,8 +39,11 @@ def draw_molecule(corners_array, graph: Graph, img):
             drawn.add(index)
             drawn_cycle_pts_dict[index] = points
         else:
-            connected_nodes = list(set(cycles[list(drawn_cycles_that_are_connected)[0]]).intersection(set(cycle)))  # used to construct line to reflect shape across
-            img = draw_connected_cycle_by_reflection(connected_nodes, drawn_cycle_pts_dict[list(drawn_cycles_that_are_connected)[0]], img)  #!! KEY ERROR IF > 2 CYCLES
+            # used to construct line to reflect shape across
+            connected_nodes = list(set(cycles[list(drawn_cycles_that_are_connected)[0]]).intersection(set(cycle)))
+            # !! KEY ERROR IF > 2 CYCLES
+            img = draw_connected_cycle_by_reflection(connected_nodes, drawn_cycle_pts_dict[
+                list(drawn_cycles_that_are_connected)[0]], img)
             drawn.add(index)
 
     cv.imshow("drawn", img)
@@ -55,12 +58,12 @@ def draw_first_cycle(cycle, corners, img, bond_length):
     cv.circle(img, center, 10, (0, 0, 255))
 
     cycle = list(cycle)
-    deg = 360/len(cycle)
+    deg = 360 / len(cycle)
     points = []
     points_dict = {}
     for i in range(len(cycle)):
-        x = bond_length*np.cos(np.deg2rad(deg*i + deg/2)) + center[0]
-        y = bond_length*np.sin(np.deg2rad(deg*i + deg/2)) + center[1]
+        x = bond_length * np.cos(np.deg2rad(deg * i + deg / 2)) + center[0]
+        y = bond_length * np.sin(np.deg2rad(deg * i + deg / 2)) + center[1]
 
         points.append([int(x), int(y)])
         points_dict[cycle[i]] = points[i]
@@ -89,15 +92,15 @@ def draw_connected_cycle_by_reflection(connected_nodes, cycle_to_flip_points, im
     cv.circle(img, tuple(pts[0]), 10, (255, 0, 0), -1)
     cv.circle(img, tuple(pts[1]), 10, (255, 0, 0), -1)
 
-    m, b, _, _, _ = stats.linregress(pts[:,0], pts[:,1])
+    m, b, _, _, _ = stats.linregress(pts[:, 0], pts[:, 1])
 
     reflected_points = []
     for key, val in cycle_to_flip_points.items():
         x, y = val
-        u = ((1 - m**2)*x + 2*m*y - 2*m*b) / (m**2 + 1)
-        v = ((m**2 - 1)*y + 2*m*x + 2*b) / (m**2 + 1)
+        u = ((1 - m ** 2) * x + 2 * m * y - 2 * m * b) / (m ** 2 + 1)
+        v = ((m ** 2 - 1) * y + 2 * m * x + 2 * b) / (m ** 2 + 1)
         if np.isnan(u) or np.isnan(v):
-            reflected_points.append((int(2*pts[0,0]-x) , int(y)))
+            reflected_points.append((int(2 * pts[0, 0] - x), int(y)))
         else:
             reflected_points.append((int(u), int(v)))
 
@@ -105,6 +108,7 @@ def draw_connected_cycle_by_reflection(connected_nodes, cycle_to_flip_points, im
     img = cv.polylines(img, [points],
                        True, (0, 255, 0), 2)
     return img
+
 
 """ 
     find two closest points to connect
